632f27814c283e0995d7dacfb2cce066ebccc8ee
Gian Luca <55563130+GLPG35@users.noreply.github.com>
2026-01-28 10:27:46 -0300

    v2.7.0
    Add dynamic meta tags

---
+++ middleware.ts
@@ -0,0 +1,56 @@
+export const config = {
+	matcher: ['/*']
+}
+
+export default async function middleware(req: Request) {
+	const url = req.url
+	const userAgent = req.headers.get('user-agent') as string
+
+	const socialMediaCrawlerUserAgents = /Twitterbot|facebookexternalhit|Facebot|LinkedInBot|Pinterestbot|Slackbot|vkShare|W3C_Validator/i
+	const isSocialMediaCrawler = socialMediaCrawlerUserAgents.test(userAgent)
+
+	if (!isSocialMediaCrawler) return
+
+	const pages = {
+		never7: {
+			title: 'Infinity Series - Never7',
+			image: 'https://infinityseri.es/never7_banner_horizontal.webp'
+		},
+		ever17: {
+			title: 'Infinity Series - Ever17',
+			image: 'https://infinityseri.es/ever17_banner_horizontal.webp'
+		},
+		remember11: {
+			title: 'Infinity Series - Remember11',
+			image: 'https://infinityseri.es/remember11_banner_horizontal.webp'
+		}
+	}
+
+	const hasSlash = url.split('/').pop() == ''
+	const splitUrl = url.split('/').pop() as string
+
+	return new Response(`
+		<html>
+		<head>
+			<meta charset="UTF-8" />
+			<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+			<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+			<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+			<link rel="manifest" href="/site.webmanifest">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<title>${hasSlash ? pages[splitUrl].title : 'Infinity Series'}</title>
+			<meta name="description" content="This story is not over yet. Because only you are in the infinity loop." />
+			<meta property="og:type" content="website" />
+			<meta property="og:title" content="${hasSlash ? pages[splitUrl].title : 'Infinity Series'}" />
+			<meta property="og:description" content="This story is not over yet. Because only you are in the infinity loop." />
+			<meta property="og:url" content="https://infinityseri.es/" />
+			<meta property="og:image" content=${hasSlash ? pages[splitUrl].image : pages[Math.floor(Math.random() * 3)].image} />
+			<meta property="twitter:card" content="summary_large_image" />
+			<meta property="twitter:url" content="https://infinityseri.es/" />
+			<meta property="twitter:title" content="${hasSlash ? pages[splitUrl].title : 'Infinity Series'}" />
+			<meta property="twitter:description" content="This story is not over yet. Because only you are in the infinity loop." />
+			<meta property="twitter:image" content=${hasSlash ? pages[splitUrl].image : pages[Math.floor(Math.random() * 3)].image} />
+		</head>
+		</html>
+	`)
+}
