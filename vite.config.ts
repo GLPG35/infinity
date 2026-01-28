b312bf8b10e45138b14f145b4b93d1c85f259681
Gian Luca <55563130+GLPG35@users.noreply.github.com>
2026-01-28 12:16:47 -0300

    v2.6.0
    Rollback

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import vercel from 'vite-plugin-vercel'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    vercel({
-      rewrites: [{ source: '/(.*)', destination: '/index.html' }],
-      middleware: './middleware.ts'
-    })
-  ],
+  plugins: [react()]
 })
